cc5fe737cfd9dab0d979411f76c0b0cafec3c154
houyu01 <houyu01@baidu.com>
2018-11-28 02:06:22 +0800

    add description in build script

--- tools/deploy-docs.sh
+++ tools/deploy-docs.sh
@@ -1,6 +1,8 @@
-echo '开始clone文档分支'
+echo '编译文档.........'
 
 node_modules/.bin/gitbook build
+
+echo '克隆文档分支.........'
 
 git checkout gh-pages
 
@@ -11,5 +13,7 @@ git add .
 git commit -m 'deploy host-app docs'
 
 git push origin gh-pages
+
+echo '部署完成切回.........'
 
 git checkout master
