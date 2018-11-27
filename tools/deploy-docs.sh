c3c120089fda6cec12f470129715a362993e55b1
houyu01 <houyu01@baidu.com>
2018-11-28 01:47:09 +0800

    adjust dir structure

---
+++ tools/deploy-docs.sh
@@ -0,0 +1,29 @@
+echo '开始clone文档分支'
+
+node_modules/.bin/gitbook build
+
+git checkout gh-pages
+
+cp -R _book/* .
+
+
+
+#git clone https://github.com/swan-team/host-app-guide.git ./docs
+
+# cd ./docs
+# 
+# git checkout gh-pages
+# 
+# cd ../
+# 
+# echo '开始编译'
+# 
+# node_modules/.bin/gitbook build ./root ./docs
+# 
+# cd ./docs
+# 
+# git add .
+# 
+# git commit -m 'deploy host-app docs'
+# 
+# git push origin gh-pages
